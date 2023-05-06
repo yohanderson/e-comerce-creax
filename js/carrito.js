656be79dc1712c023f4805b526cbf5298e9881f0
Yohanderson <yohandersonmarquez@outlook.es>
2023-05-06 15:30:41 -0400

    creax lista para publicar

--- js/carrito.js
+++ js/carrito.js
@@ -1,5 +1,8 @@
 var link = document.querySelector('link[rel="shortcut icon"]');
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+if (
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
   // El navegador está en modo oscuro
   link.href = "./source/icon-blanco.png";
 } else {
@@ -18,24 +21,25 @@ let buttonsDelete = document.querySelectorAll(".cart-product-delete");
 const buttonEmpty = document.querySelector("#cart-actions-empty");
 const containerTotal = document.querySelector("#total");
 const buttonBuy = document.querySelector("#cart-actions-buy");
-
+const cartTableSpecifications = document.querySelector(".cart-table-specifications");
 
 function loadProductsCart() {
-    if (productsInCart && productsInCart.length > 0) {
+  if (productsInCart && productsInCart.length > 0) {
+    cartTableSpecifications.classList.remove("disabled");
+    containerCartEmpty.classList.add("disabled");
+    containerCartProducts.classList.remove("disabled");
+    containerCartActions.classList.remove("disabled");
+    containerCartBuying.classList.add("disabled");
 
-        containerCartEmpty.classList.add("disabled");
-        containerCartProducts.classList.remove("disabled");
-        containerCartActions.classList.remove("disabled");
-        containerCartBuying.classList.add("disabled");
-    
-        containerCartProducts.innerHTML = "";
-    
-        productsInCart.forEach(product => {
-    
-            const div = document.createElement("div");
-            div.classList.add("cart-product");
-            div.innerHTML = `
-                <img class="cart-product-image" src="${product.image}" alt="${product.titule}">
+    containerCartProducts.innerHTML = "";
+
+    productsInCart.forEach((product) => {
+      const div = document.createElement("div");
+      div.classList.add("cart-product");
+      div.innerHTML = `
+                <img class="cart-product-image" src="${product.image}" alt="${
+        product.titule
+      }">
                 <div class="cart-product-titule">
                     <h3>${product.titule}</h3>
                 </div>
@@ -48,100 +52,103 @@ function loadProductsCart() {
                 <div class="cart-product-subtotal">
                     <p>$${product.price * product.amount}</p>
                 </div>
-                <button class="cart-product-delete" id="${product.id}"><i class="bi bi-trash-fill"></i></button>
+                <button class="cart-product-delete" id="${
+                  product.id
+                }"><i class="bi bi-trash-fill"></i></button>
             `;
-    
-            containerCartProducts.append(div);
-        })
-    
+
+      containerCartProducts.append(div);
+    });
+
     updateButtonsDelete();
     updateTotal();
-	
-    } else {
-        containerCartEmpty.classList.remove("disabled");
-        containerCartProducts.classList.add("disabled");
-        containerCartActions.classList.add("disabled");
-        containerCartBuying.classList.add("disabled");
-    }
-
+  } else {
+    cartTableSpecifications.classList.add("disabled");
+    containerCartEmpty.classList.remove("disabled");
+    containerCartProducts.classList.add("disabled");
+    containerCartActions.classList.add("disabled");
+    containerCartBuying.classList.add("disabled");
+  }
 }
 
 loadProductsCart();
 
 function updateButtonsDelete() {
-    buttonsDelete = document.querySelectorAll(".cart-product-delete");
+  buttonsDelete = document.querySelectorAll(".cart-product-delete");
 
-    buttonsDelete.forEach(button => {
-        button.addEventListener("click", removeFromCart);
-    });
+  buttonsDelete.forEach((button) => {
+    button.addEventListener("click", removeFromCart);
+  });
 }
 
 function removeFromCart(e) {
-    Toastify({
-        text: "Producto eliminado",
-        duration: 3000,
-        close: true,
-        gravity: "top",
-        position: "right",
-        stopOnFocus: true, 
-        style: {
-          background: "linear-gradient(to right, #4b33a8, #785ce9)",
-          borderRadius: "2rem",
-          textTransform: "uppercase",
-          fontSize: ".75rem"
-        },
-        offset: {
-            x: '1.5rem', 
-            y: '1.5rem'
-          },
-        onClick: function(){}
-      }).showToast();
+  Toastify({
+    text: "Producto eliminado",
+    duration: 3000,
+    close: true,
+    gravity: "top",
+    position: "right",
+    stopOnFocus: true,
+    style: {
+      background: "linear-gradient(to right, #4b33a8, #785ce9)",
+      borderRadius: "2rem",
+      textTransform: "uppercase",
+      fontSize: ".75rem",
+    },
+    offset: {
+      x: "1.5rem",
+      y: "1.5rem",
+    },
+    onClick: function () {},
+  }).showToast();
 
-    const idBoton = e.currentTarget.id;
-    const index = productsInCart.findIndex(product => product.id === idButton);
-    
-    productsInCart.splice(index, 1);
-    loadProductsCart();
+  const idButton = e.currentTarget.id;
+  const index = productsInCart.findIndex((product) => product.id === idButton);
 
-    localStorage.setItem("products-In-cart", JSON.stringify(productsInCart));
+  productsInCart.splice(index, 1);
+  loadProductsCart();
 
+  localStorage.setItem("products-In-cart", JSON.stringify(productsInCart));
 }
 
 buttonEmpty.addEventListener("click", emptyCart);
 function emptyCart() {
-
-    Swal.fire({
-        title: '¿Estás seguro?',
-        icon: 'question',
-        html: `Se van a borrar ${productsInCart.reduce((acc, product) => acc + product.amount, 0)} products.`,
-        showCancelButton: true,
-        focusConfirm: false,
-        confirmButtonText: 'Sí',
-        cancelButtonText: 'No'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            productsInCart.length = 0;
-            localStorage.setItem("products-in-cart", JSON.stringify(productsInCart));
-            loadProductsCart();
-        }
-      })
+  Swal.fire({
+    title: "¿Estás seguro?",
+    icon: "question",
+    html: `Se van a borrar ${productsInCart.reduce(
+      (acc, product) => acc + product.amount,
+      0
+    )} products.`,
+    showCancelButton: true,
+    focusConfirm: false,
+    confirmButtonText: "Sí",
+    cancelButtonText: "No",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      productsInCart.length = 0;
+      localStorage.setItem("products-in-cart", JSON.stringify(productsInCart));
+      loadProductsCart();
+    }
+  });
 }
 
-
 function updateTotal() {
-    const totalCalculator = productsInCart.reduce((acc, product) => acc + (product.price * product.amount), 0);
-    total.innerText = `$${totalCalculator}`;
+  const totalCalculator = productsInCart.reduce(
+    (acc, product) => acc + product.price * product.amount,
+    0
+  );
+  total.innerText = `$${totalCalculator}`;
 }
 
 buttonBuy.addEventListener("click", buyCart);
 function buyCart() {
+  productsInCart.length = 0;
+  localStorage.setItem("products-in-cart", JSON.stringify(productsInCart));
 
-    productsInCart.length = 0;
-    localStorage.setItem("products-in-cart", JSON.stringify(productsInCart));
-    
-    containerCartEmpty.classList.add("disabled");
-    containerCartProducts.classList.add("disabled");
-    containerCartActions.classList.add("disabled");
-    containerCartBuying.classList.remove("disabled");
-
+  cartTableSpecifications.classList.add("disabled");
+  containerCartEmpty.classList.add("disabled");
+  containerCartProducts.classList.add("disabled");
+  containerCartActions.classList.add("disabled");
+  containerCartBuying.classList.remove("disabled");
 }
